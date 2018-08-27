48b12498851b0e432bec1308ec0d294aa9fd2376
Aramuad <samcballard@gmail.com>
2018-08-27 16:07:36 -0600

    add hole_2

---
+++ hole_1.rb
@@ -0,0 +1,15 @@
+def hole_1
+  a = [419, 420, 311]
+  sum = 0
+  a.each { |a| sum+=a }
+  puts sum
+end
+
+hole_1
+
+def hole_2
+  user_choice = gets.strip
+  comp_choice = ['rock', 'paper', 'scissors'].sample
+  case user_choice
+    if user_choice = rock
+end
